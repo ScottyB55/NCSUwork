2293f09f986a1cf6d54549e9e6448a73135dae66
Scott Burnett <srburne2@grendel27.ece.ncsu.edu>
2022-02-04 16:42:28 -0500

    Submitting lab_002_partb

--- labs/lab_002/lab_002_partb.v
+++ labs/lab_002/lab_002_partb.v
@@ -5,9 +5,17 @@ module lab_002_partb (
   output F
 );
 
-  /* put your variable declarations here */
+  // Dataflow Verilog
+  // assign F = A&B&C | ~A&B;
 
+  // Structural Verilog
+  /* put your variable declarations here */
+  wire notA, nAB, ABC;
 
   /* put your gate instances here */
-
+  not U1 (notA, A);
+  and U2 (nAB, notA, B);
+  and U3 (ABC, A, B, C);
+  or  U4 (F, nAB, ABC);
+ 
 endmodule
