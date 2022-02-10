9dcbe2deb847de7364e3c276fad4bdd3e5b9e972
Scott Burnett <srburne2@grendel31.ece.ncsu.edu>
2022-02-09 19:21:37 -0500

    Lab 3 design files

--- labs/lab_003/lab_003_partc.v
+++ labs/lab_003/lab_003_partc.v
@@ -2,5 +2,13 @@ module lab_003_partc (
   input A, B, C, D,
   output F
 );
+  wire G;	// Intermediate wire
+
+  // Instantiate other modules
+  // <modulename> <reference> (<portconnections>)
+  // portconnection order defined in module definition
+  // Can be explicitly defined
+  lab_003_parta PARTA(.G(G), .A(A), .B(B), .C(C));
+  lab_003_partb PARTB(.F(F), .G(G), .D(D));
 
 endmodule
